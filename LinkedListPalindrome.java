5f1feeb3bcf213d37ae80cb839eb81869c965ff8
nancysharma02 <122221155+nancysharma02@users.noreply.github.com>
2023-01-27 23:23:49 +0530

    Add files via upload

---
+++ LinkedListPalindrome.java
@@ -0,0 +1,31 @@
+public class LinkedListPalindrome
+{	
+	static boolean isPalindrome(int n)
+	{
+
+		int divisor = 1;
+		while (n / divisor >= 10)
+			divisor *= 10;
+	
+		while (n != 0)
+		{
+			int leading = n / divisor;
+			int trailing = n % 10;
+	
+			if (leading != trailing)
+				return false;
+				n = (n % divisor) / 10;
+	
+			divisor = divisor / 100;
+		}
+		return true;
+	}
+	public static void main(String args[])
+	{
+		if(isPalindrome(12321))
+			System.out.println("Yes, it is Palindrome");
+		else
+			System.out.println("No, not Palindrome");
+	}
+}
+
